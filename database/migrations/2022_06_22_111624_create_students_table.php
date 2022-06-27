e1e94a0f34000f16cf84334339a84f2fd8a3968f
Paritosh Gupta <69110353+paritoshg25@users.noreply.github.com>
2022-06-27 14:35:37 +0530

    updates

--- database/migrations/2022_06_22_111624_create_students_table.php
+++ database/migrations/2022_06_22_111624_create_students_table.php
@@ -26,6 +26,10 @@ class CreateStudentsTable extends Migration
             // $table->timestamp('created_time')->useCurrent();
             // $table->timestamp('updated_time')->nullable()->useCurrentOnUpdate();
         });
+
+        Schema::table('students', function (Blueprint $table) {
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
     }
 
     /**
